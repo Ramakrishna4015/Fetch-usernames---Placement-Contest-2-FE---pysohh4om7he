7108d11a39ef63c5249ec5acfca218e1f896f6d3
Ramakrishna4015 <ramakrishna.kodimoju@gmail.com>
2022-10-09 15:44:56 +0530

    app.jsedit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useSyncExternalStore } from 'react'
 import '../styles/App.css';
 import { useState, useEffect } from 'react';
 const App = () => {
 //code here 
- 
+ const [name, setName] =useState('');
+ const [id, setId] = useState(1);
+
+ useEffect(() =>{
+  fetch('https:content.newtonschool.co/v1/pr/main/users/${id}')
+  .then((resp)=>resp.json()).then((data) => {
+    console.log(data.name);
+    setName(data.name);
+  })
+ },[id])
+ const changeInput=(e) =>{
+  setId(e.target.value)
+ }
 
 
 
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
-      <input id="input" onChange={changeInput} />
+      <input id="input" onChange={changeInput(e)} />
       <p id="name">{name}</p>
     </div>
   );
